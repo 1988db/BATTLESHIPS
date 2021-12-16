6e10f30bee3bd9a28d527d80735ace1980fbeb57
1988db <elozrana@gmail.com>
2021-12-16 23:56:59 +0100

    generate function add

--- app.js
+++ app.js
@@ -75,6 +75,15 @@ document.addEventListener('DOMContentLoaded', ()=> {
         let current = ship.directions[randomDirection];
         if(randomDirection === 0) direction = 1;
         if(randomDirection === 1) direction = 10;
-        let randomStart = Math.floor(Math.random() * computerSquares.length);
+        let randomStart = Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction));
+
+        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'));
+        const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1);
+        const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0);
+
+        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name))
+        else generate(ship);
     }
+
+    generate(shipsArray[0])
 })
